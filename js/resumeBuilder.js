fe132bba4a2dacc3d28076ea82d1f7b2b1ecb8c3
bettertoday <bettertoday@gmail.com>
2017-09-21 00:08:17 +0900

    ..

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ $("#main").append("Insoo Kim");
+ var firstName = "Insoo";
+ var age = 47;
+ console.log(firstName);
