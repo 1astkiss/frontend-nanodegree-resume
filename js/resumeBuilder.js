87712080c79122853dfa8787bdc8b4bbce0db522
bettertoday <bettertoday@gmail.com>
2017-09-24 00:09:40 +0900

    ..

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,17 +1,17 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
 
- $("#main").append("Insoo Kim");
- $("#main").append(" is awesome.");
- $("#main").append(" is awesome.");
- var firstName = "Insoo";
- var age = 47;
- console.log(firstName);
+$("#main").append("Insoo Kim");
+$("#main").append(" is awesome.");
+$("#main").append(" is awesome.");
+var firstName = "Insoo";
+var age = 47;
+console.log(firstName);
 
- var email="lastkiss@lastkiss.net";
- var name="Insoo Kim";
- var role="Developer";
+var email="lastkiss@lastkiss.net";
+var name="Insoo Kim";
+var role="Developer";
  // var newEmail = email.replace("lastkiss", "gmail");
  console.log(email);
  var formattedName = HTMLheaderName.replace("%data%", name);
@@ -22,9 +22,9 @@ This is empty on purpose! Your code to build the resume will go here.
  var s = "audacity";
 
  var udacityizer = function(s) {  
-    s=s.slice(1);
-    s=s.charAt(0).toUpperCase() + s.slice(1);
-    return s;
+ 	s=s.slice(1);
+ 	s=s.charAt(0).toUpperCase() + s.slice(1);
+ 	return s;
  };
 
 // Did your code work? The line below will tell you!
@@ -33,7 +33,7 @@ console.log(udacityizer(s));
 var sampleArray = [0,0,7];
 
 var incrementLastArrayElement = function(_array) {
-    var newArray = [];
+	var newArray = [];
     // Your code should make newArray equal to an array that has the same
     // values as _array, but the last number has increased by one.
     
@@ -45,7 +45,7 @@ var incrementLastArrayElement = function(_array) {
     // Your code goes in here!
 
     for(var i=0;i<_array.length; i++){
-        newArray[i] = _array[i];
+    	newArray[i] = _array[i];
     }
     
     newArray[_array.length - 1] += 1;
@@ -68,9 +68,9 @@ var incrementLastArrayElement = function(_array) {
 //     var firstName = names[0].toLowerCase().charAt(0).toUpperCase() 
 //     + names[0].toLowerCase().slice(1);
 //     var lastName = names[1].toUpperCase();
-    
+
 //     finalName = firstName + " " + lastName;
-    
+
 //     // Don't delete this line!
 //     return finalName;
 // };
@@ -92,6 +92,37 @@ var bio = {
 }
 
 var formattedContact 
-		= HTMLcontactGeneric.replace("%data%", bio.contacts.email)
-				.replace("%contact%", "email");
+= HTMLcontactGeneric.replace("%data%", bio.contacts.email)
+.replace("%contact%", "email");
 $("#topContacts").append(formattedContact);
+
+var work = {};
+work.position = "Founder";
+work.employer = "Fixlab Korea";
+work.yearsWorked = 2;
+
+var project ={};
+project["lastSchoolAttended"] = "INSEAD";
+project["yearsAttended"] = 5;
+project["schoolCity"] = "Fontainedbleu, France";
+
+console.log(work.yearsWorked);
+$("#topContacts").append(work.position).append(project.lastSchoolAttended);
+
+var education = {
+	"schools":[
+		{
+			"name" : "INSEAD",
+			"city" : "Fontainedbleu, France",
+			"major" : "MBA",
+			"gradYear" : 2005
+		},
+
+		{
+			"name" : "SNU",
+			"city" : "Seoul, Korea",
+			"major" : "Lanscape Architecture",
+			"gradYear" : 1995
+		}
+	]
+}
